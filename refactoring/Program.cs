3807819e3b233c0bcb0cfcb22e1d4e5925e8b9b9
AsylbekA <anarbayasylbek@gmail.com>
2020-10-24 23:44:08 +0600

    111

--- refactoring/Program.cs
+++ refactoring/Program.cs
@@ -45,7 +45,11 @@ namespace refactoring
         {
             for (int i=0;i<=n;i++)
             {
-
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
             }
         }
         #endregion
